2947da77b25bb470e6ceb306a407809d8eec31d8
GeorgiyKuzora <g.kuzora@korona.net>
2025-10-21 13:57:06 +0300

    Finish far to cel

--- far_cel/src/main.rs
+++ far_cel/src/main.rs
@@ -1,31 +1,55 @@
 use std::io;
 
 fn main() {
-    println!("What convertion you want?:\n1. far->cel\n2. cel->far\nEnter a number:");
 
-    let result: i32;
+    println!("What convertion you want?:\n1. far->cel\n2. cel->far\nEnter a number:");
+    let flow: u32;
     loop {
         let mut buf = String::new();
         io::stdin()
             .read_line(&mut buf)
             .expect("Error");
-        result = match buf.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+        flow = match buf.trim().parse() {
+            Ok(num) => {
+                match num {
+                    1 => 1,
+                    2 => 2,
+                    _ => {
+                        println!("Enter a valid number");
+                        continue;
+                    },
+                }
+            },
+            Err(_) => {
+                println!("Enter a valid number");
+                continue;
+            },
         };
-        let temp = match result {
-            1 => convert_far_to_cel(result),
-            2 => convert_cel_to_far(result),
-            _ => continue,
-        };
-
-        match result {
-            1 => println!("{}"),
-            2 => convert_cel_to_far(result),
-            _ => break,
-        }
         break;
     }
+
+    println!("Write a value to convert:");
+    let value: i32;
+    loop {
+        let mut buf = String::new();
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("Error");
+        value = match buf.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a valid number");
+                continue;
+            },
+        };
+        break;
+    }
+    let temp = match flow {
+        1 => convert_far_to_cel(value),
+        2 => convert_cel_to_far(value),
+        _ => 0,
+    };
+    println!("value = {value}, temp = {temp}");
 }
 
 fn convert_far_to_cel(far: i32) -> i32 {
